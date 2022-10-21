1f9995eee8ac33185e101524ec25e27a5e6a33e0
Sergio Rendon <56836232+SergioR99@users.noreply.github.com>
2022-10-20 19:08:20 -0500

    validaciones

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 const bottonConfirn = document.getElementById('boton-confirn')
 
-function traerValor() {
-    const cardNumber = document.getElementById('card-number').value
-    document.getElementById('card-number-text').innerHTML = cardNumber
-
+function inputVacio(value) {
     const cardName = document.getElementById('card-name').value
-    document.getElementById('card-name-text').innerHTML = cardName
+
+    if (value.length == 0 || /^\s+$/.test(value)) {
+        console.log('Ingrese dato');
+        return false
+    }
+    return true;
+}
+
+console.log(document.getElementById('formulario'));
+const form = new FormData (
+    document.getElementById('formulario')
+)
+
+console.log(form.entries());
+
+bottonConfirn.addEventListener('click', inputVacio)
+
+function traerValor() {
+    const cardName = document.getElementById('card-name').value
+    const cardNameText = document.getElementById('card-name-text');
+    cardNameText.innerHTML = cardName.length > 0 ? cardName : cardNameText;
+
+    const cardNumber = document.getElementById('card-number').value
+    const cardNumberText = document.getElementById('card-number-text')
+    cardNumberText.innerHTML = cardNumber.length > 0 ? cardNumber : cardNumberText.innerHTML;
 
     const cardCvc = document.getElementById('card-cvc').value
     document.getElementById('card-cvc-text').innerHTML = cardCvc
@@ -15,6 +36,8 @@ function traerValor() {
 
     const cardAnio = document.getElementById('card-anio').value
     document.getElementById('card-anio-text').innerHTML = cardAnio
+
+    console.log('entra');
 }
 
-bottonConfirn.addEventListener('click', traerValor)
+// bottonConfirn.addEventListener('click', traerValor)
